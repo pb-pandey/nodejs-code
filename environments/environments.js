428a20685db4ebc232ff9bf4c5389d7897c5bb1a
Rajesh Kumar <rajesh16a1051@gmail.com>
2024-02-10 20:45:45 +0530

    en

---
+++ environments/environments.js
@@ -0,0 +1,18 @@
+env={};
+env.mongodbHostname =`mongodb+srv://ppmongodb:29lhcPS2Q1k3ahpp@cluster0.a6pv7bu.mongodb.net/`
+env.mongodbPort = '27017';
+env.userName = 'ppmongodb'
+env.pass = '29lhcPS2Q1k3ahpp'
+env.mongodb = 'performsmile'
+
+
+// r2 - cloudflare
+env.accountId = '166095adb224dcbcabfc5fb19f4a61cd';
+env.accesskey = '3cf0620ec8b202221d12287a42a07c51';
+env.secretkey = '48313699580ca9ef5ff636b0f4fa2528ccd9da4a916edc37b04d6fc70b83f78c';
+
+env.ipAddress = 'localhost'//'18.211.196.103';//'3.6.132.105'; //3.6.192.83
+
+
+
+module.exports = env;
