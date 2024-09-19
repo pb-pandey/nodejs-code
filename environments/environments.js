40cd64dec3e408bf06652460a154daa0858f962d
pb-pandey <159260614+pb-pandey@users.noreply.github.com>
2024-09-19 23:30:14 +0530

    Update environments.js

--- environments/environments.js
+++ environments/environments.js
@@ -11,7 +11,7 @@ env.accountId = '166095adb224dcbcabfc5fb19f4a61cd';
 env.accesskey = '3cf0620ec8b202221d12287a42a07c51';
 env.secretkey = '48313699580ca9ef5ff636b0f4fa2528ccd9da4a916edc37b04d6fc70b83f78c';
 
-env.ipAddress = '13.213.90.156'//'18.211.196.103';//'3.6.132.105'; //3.6.192.83
+env.ipAddress = '3.111.209.65'//'18.211.196.103';//'3.6.132.105'; //3.6.192.83
 
 
 
